f27dbc4bbb085cfbfcece7576719c2f19192d644
jiyeong99 <1999wldud@gmail.com>
2022-08-01 15:07:03 +0900

    savefile

--- 1회차/김지영/0_최빈수구하기.py
+++ 1회차/김지영/0_최빈수구하기.py
@@ -1,3 +1,16 @@
 import sys
 
 sys.stdin = open("_최빈수구하기.txt")
+T = int(input())
+for test_case in range(1,T+1):
+
+    testcase = int(input())
+    score_list = list(map(int,input().split()))
+    score = {}
+    for i in range(1000):
+        if score_list[i] not in score:
+            score[score_list[i]] = 1
+        else : score[score_list[i]] += 1
+    result = max(score,key=score.get)
+    
+    print(f'#{testcase}',result)
--- 1회차/김지영/1_소득불균형.py
+++ 1회차/김지영/1_소득불균형.py
@@ -1,3 +1,14 @@
 import sys
 
 sys.stdin = open("_소득불균형.txt")
+T = int(input())
+for test_case in range(1,T+1):
+    n = int(input())
+    numbers = list(map(int,input().split()))
+    avg = sum(numbers)/n
+    c = 0
+    for i in range(n):
+        if numbers[i] <= avg:
+            c += 1
+    result = c
+    print(f'#{test_case}',result)
